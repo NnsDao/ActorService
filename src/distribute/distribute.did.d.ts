2666ecd067e706f556972fe723231f1c82f14693
neptune <103205025+neptune0xf@users.noreply.github.com>
2022-07-08 16:05:21 +0800

    distribute service

---
+++ src/distribute/distribute.did.d.ts
@@ -0,0 +1,15 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export type Result = { 'Ok' : bigint } |
+  { 'Err' : string };
+export type Result_1 = { 'Ok' : null } |
+  { 'Err' : string };
+export interface _SERVICE {
+  'add_white_list_bulk' : ActorMethod<[Array<[string, bigint]>], Result>,
+  'get_err_log' : ActorMethod<[], Array<string>>,
+  'get_owner' : ActorMethod<[], Array<Principal>>,
+  'get_success_log' : ActorMethod<[], Array<string>>,
+  'get_white_list' : ActorMethod<[], Array<[string, bigint]>>,
+  'try_exchange' : ActorMethod<[], Result_1>,
+}
---
+++ src/distribute/distribute.did.js
@@ -0,0 +1,21 @@
+export const idlFactory = ({ IDL }) => {
+  const Result = IDL.Variant({ 'Ok' : IDL.Nat64, 'Err' : IDL.Text });
+  const Result_1 = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text });
+  return IDL.Service({
+    'add_white_list_bulk' : IDL.Func(
+        [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Nat64))],
+        [Result],
+        [],
+      ),
+    'get_err_log' : IDL.Func([], [IDL.Vec(IDL.Text)], ['query']),
+    'get_owner' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'get_success_log' : IDL.Func([], [IDL.Vec(IDL.Text)], ['query']),
+    'get_white_list' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Nat64))],
+        ['query'],
+      ),
+    'try_exchange' : IDL.Func([], [Result_1], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
