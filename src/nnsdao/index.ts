5e0bde2aabd6e27620f006069b2656e4801c44d6
neptune0xf <null>
2022-10-27 06:51:10 +0000

    🔄 Synced file(s) with NnsDao/nnsdao

--- src/nnsdao/index.ts
+++ src/nnsdao/index.ts
@@ -1,19 +1,90 @@
 export const idlFactory = ({ IDL }) => {
-  const StackingItem = IDL.Record({
-    principal: IDL.Opt(IDL.Principal),
-    duration: IDL.Nat8,
-    amount: IDL.Nat64
+  const DaoInfo = IDL.Record({
+    option: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))),
+    name: IDL.Text,
+    tags: IDL.Vec(IDL.Text),
+    intro: IDL.Text,
+    avatar: IDL.Text,
+    poster: IDL.Text
   });
-  const Result = IDL.Variant({ Ok: IDL.Int32, Err: IDL.Text });
+  const Result = IDL.Variant({ Ok: DaoInfo, Err: IDL.Text });
+  const Votes = IDL.Variant({ No: IDL.Nat64, Yes: IDL.Nat64 });
+  const ProposalState = IDL.Variant({
+    Failed: IDL.Text,
+    Open: IDL.Null,
+    Executing: IDL.Null,
+    Rejected: IDL.Null,
+    Succeeded: IDL.Null,
+    Accepted: IDL.Null
+  });
+  const Proposal = IDL.Record({
+    id: IDL.Nat64,
+    title: IDL.Text,
+    content: IDL.Text,
+    vote_data: IDL.Vec(IDL.Tuple(IDL.Principal, Votes)),
+    end_time: IDL.Nat64,
+    timestamp: IDL.Nat64,
+    property: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))),
+    proposer: IDL.Principal,
+    proposal_state: ProposalState
+  });
+  const Result_1 = IDL.Variant({ Ok: Proposal, Err: IDL.Text });
+  const Result_2 = IDL.Variant({
+    Ok: IDL.Vec(IDL.Tuple(IDL.Nat64, Proposal)),
+    Err: IDL.Text
+  });
+  const Social = IDL.Record({ key: IDL.Text, link: IDL.Text });
+  const JoinDaoParams = IDL.Record({
+    nickname: IDL.Text,
+    social: IDL.Vec(Social),
+    intro: IDL.Text,
+    avatar: IDL.Text
+  });
+  const MemberItems = IDL.Record({
+    principal: IDL.Principal,
+    nickname: IDL.Text,
+    social: IDL.Vec(Social),
+    intro: IDL.Text,
+    status_code: IDL.Int8,
+    avatar: IDL.Text
+  });
+  const Result_3 = IDL.Variant({ Ok: MemberItems, Err: IDL.Text });
+  const Result_4 = IDL.Variant({
+    Ok: IDL.Vec(MemberItems),
+    Err: IDL.Text
+  });
+  const Result_5 = IDL.Variant({ Ok: IDL.Text, Err: IDL.Text });
+  const ProposalLog = IDL.Record({
+    pending: IDL.Vec(IDL.Nat64),
+    finished: IDL.Vec(IDL.Tuple(IDL.Nat64, Result_5))
+  });
+  const Result_6 = IDL.Variant({ Ok: ProposalLog, Err: IDL.Text });
+  const ProposalContent = IDL.Record({
+    title: IDL.Text,
+    content: IDL.Text,
+    end_time: IDL.Nat64,
+    property: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)))
+  });
+  const UserVoteArgs = IDL.Record({
+    id: IDL.Nat64,
+    principal: IDL.Opt(IDL.Principal),
+    vote: Votes
+  });
+  const Result_7 = IDL.Variant({ Ok: IDL.Null, Err: IDL.Text });
   return IDL.Service({
-    stacking_list: IDL.Func(
-      [],
-      [IDL.Vec(IDL.Tuple(IDL.Nat64, StackingItem))],
-      ['query']
-    ),
-    test: IDL.Func([], [Result], ['query'])
+    dao_info: IDL.Func([], [Result], []),
+    get_proposal: IDL.Func([IDL.Nat64], [Result_1], ['query']),
+    get_proposal_list: IDL.Func([], [Result_2], ['query']),
+    join: IDL.Func([JoinDaoParams], [Result_3], []),
+    member_list: IDL.Func([], [Result_4], []),
+    proposal_heartbeat_log: IDL.Func([], [Result_6], ['query']),
+    propose: IDL.Func([ProposalContent], [Result_1], []),
+    quit: IDL.Func([], [Result_3], []),
+    update_dao_info: IDL.Func([DaoInfo], [Result], []),
+    user_info: IDL.Func([IDL.Opt(IDL.Principal)], [Result_3], []),
+    vote: IDL.Func([UserVoteArgs], [Result_7], [])
   });
 };
 export const init = ({ IDL }) => {
-  return [IDL.Principal];
+  return [];
 };
--- src/nnsdao/types.ts
+++ src/nnsdao/types.ts
@@ -1,13 +1,84 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export type Result = { Ok: number } | { Err: string };
-export interface StackingItem {
-  principal: [] | [Principal];
-  duration: number;
-  amount: bigint;
+export interface DaoInfo {
+  option: [] | [Array<[string, string]>];
+  name: string;
+  tags: Array<string>;
+  intro: string;
+  avatar: string;
+  poster: string;
 }
+export interface JoinDaoParams {
+  nickname: string;
+  social: Array<Social>;
+  intro: string;
+  avatar: string;
+}
+export interface MemberItems {
+  principal: Principal;
+  nickname: string;
+  social: Array<Social>;
+  intro: string;
+  status_code: number;
+  avatar: string;
+}
+export interface Proposal {
+  id: bigint;
+  title: string;
+  content: string;
+  vote_data: Array<[Principal, Votes]>;
+  end_time: bigint;
+  timestamp: bigint;
+  property: [] | [Array<[string, string]>];
+  proposer: Principal;
+  proposal_state: ProposalState;
+}
+export interface ProposalContent {
+  title: string;
+  content: string;
+  end_time: bigint;
+  property: [] | [Array<[string, string]>];
+}
+export interface ProposalLog {
+  pending: Array<bigint>;
+  finished: Array<[bigint, Result_5]>;
+}
+export type ProposalState =
+  | { Failed: string }
+  | { Open: null }
+  | { Executing: null }
+  | { Rejected: null }
+  | { Succeeded: null }
+  | { Accepted: null };
+export type Result = { Ok: DaoInfo } | { Err: string };
+export type Result_1 = { Ok: Proposal } | { Err: string };
+export type Result_2 = { Ok: Array<[bigint, Proposal]> } | { Err: string };
+export type Result_3 = { Ok: MemberItems } | { Err: string };
+export type Result_4 = { Ok: Array<MemberItems> } | { Err: string };
+export type Result_5 = { Ok: string } | { Err: string };
+export type Result_6 = { Ok: ProposalLog } | { Err: string };
+export type Result_7 = { Ok: null } | { Err: string };
+export interface Social {
+  key: string;
+  link: string;
+}
+export interface UserVoteArgs {
+  id: bigint;
+  principal: [] | [Principal];
+  vote: Votes;
+}
+export type Votes = { No: bigint } | { Yes: bigint };
 export interface _SERVICE {
-  stacking_list: ActorMethod<[], Array<[bigint, StackingItem]>>;
-  test: ActorMethod<[], Result>;
+  dao_info: ActorMethod<[], Result>;
+  get_proposal: ActorMethod<[bigint], Result_1>;
+  get_proposal_list: ActorMethod<[], Result_2>;
+  join: ActorMethod<[JoinDaoParams], Result_3>;
+  member_list: ActorMethod<[], Result_4>;
+  proposal_heartbeat_log: ActorMethod<[], Result_6>;
+  propose: ActorMethod<[ProposalContent], Result_1>;
+  quit: ActorMethod<[], Result_3>;
+  update_dao_info: ActorMethod<[DaoInfo], Result>;
+  user_info: ActorMethod<[[] | [Principal]], Result_3>;
+  vote: ActorMethod<[UserVoteArgs], Result_7>;
 }
