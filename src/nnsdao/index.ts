16da1e9945883445631c40c64d60e0b533f13746
neptune0xf <null>
2022-12-18 02:12:38 +0000

    🔄 Synced file(s) with NnsDao/nnsdao

--- src/nnsdao/index.ts
+++ src/nnsdao/index.ts
@@ -9,7 +9,37 @@ export const idlFactory = ({ IDL }) => {
     poster: IDL.Text
   });
   const Result = IDL.Variant({ Ok: DaoInfo, Err: IDL.Text });
-  const Result_1 = IDL.Variant({ Ok: IDL.Text, Err: IDL.Text });
+  const Status = IDL.Variant({
+    stopped: IDL.Null,
+    stopping: IDL.Null,
+    running: IDL.Null
+  });
+  const DefiniteCanisterSettings = IDL.Record({
+    freezing_threshold: IDL.Nat,
+    controllers: IDL.Vec(IDL.Principal),
+    memory_allocation: IDL.Nat,
+    compute_allocation: IDL.Nat
+  });
+  const CanisterStatusResponse = IDL.Record({
+    status: Status,
+    memory_size: IDL.Nat,
+    cycles: IDL.Nat,
+    settings: DefiniteCanisterSettings,
+    module_hash: IDL.Opt(IDL.Vec(IDL.Nat8))
+  });
+  const RejectionCode = IDL.Variant({
+    NoError: IDL.Null,
+    CanisterError: IDL.Null,
+    SysTransient: IDL.Null,
+    DestinationInvalid: IDL.Null,
+    Unknown: IDL.Null,
+    SysFatal: IDL.Null,
+    CanisterReject: IDL.Null
+  });
+  const Result_1 = IDL.Variant({
+    Ok: IDL.Tuple(CanisterStatusResponse),
+    Err: IDL.Tuple(RejectionCode, IDL.Text)
+  });
   const Votes = IDL.Variant({ No: IDL.Nat64, Yes: IDL.Nat64 });
   const ProposalState = IDL.Variant({
     Failed: IDL.Text,
@@ -25,6 +55,7 @@ export const idlFactory = ({ IDL }) => {
     content: IDL.Text,
     vote_data: IDL.Vec(IDL.Tuple(IDL.Principal, Votes)),
     end_time: IDL.Nat64,
+    start_time: IDL.Nat64,
     timestamp: IDL.Nat64,
     property: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))),
     proposer: IDL.Principal,
@@ -48,6 +79,7 @@ export const idlFactory = ({ IDL }) => {
     social: IDL.Vec(Social),
     join_at: IDL.Nat64,
     intro: IDL.Text,
+    last_visit_at: IDL.Nat64,
     status_code: IDL.Int8,
     avatar: IDL.Text
   });
@@ -56,10 +88,17 @@ export const idlFactory = ({ IDL }) => {
     Ok: IDL.Vec(MemberItems),
     Err: IDL.Text
   });
+  const Result_6 = IDL.Variant({ Ok: IDL.Text, Err: IDL.Text });
+  const ProposalLog = IDL.Record({
+    pending: IDL.Vec(IDL.Nat64),
+    finished: IDL.Vec(IDL.Tuple(IDL.Nat64, Result_6))
+  });
+  const Result_7 = IDL.Variant({ Ok: ProposalLog, Err: IDL.Text });
   const ProposalContent = IDL.Record({
     title: IDL.Text,
     content: IDL.Text,
     end_time: IDL.Nat64,
+    start_time: IDL.Nat64,
     property: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)))
   });
   const UserVoteArgs = IDL.Record({
@@ -67,23 +106,21 @@ export const idlFactory = ({ IDL }) => {
     principal: IDL.Opt(IDL.Principal),
     vote: Votes
   });
-  const Result_6 = IDL.Variant({ Ok: IDL.Null, Err: IDL.Text });
+  const Result_8 = IDL.Variant({ Ok: IDL.Null, Err: IDL.Text });
   return IDL.Service({
     dao_info: IDL.Func([], [Result], []),
-    get_handled_proposal: IDL.Func(
-      [],
-      [IDL.Vec(IDL.Tuple(IDL.Nat64, Result_1))],
-      ['query']
-    ),
+    dao_status: IDL.Func([], [Result_1], []),
+    get_owner: IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
     get_proposal: IDL.Func([IDL.Nat64], [Result_2], ['query']),
     get_proposal_list: IDL.Func([], [Result_3], ['query']),
     join: IDL.Func([JoinDaoParams], [Result_4], []),
     member_list: IDL.Func([], [Result_5], []),
+    proposal_heartbeat_log: IDL.Func([], [Result_7], ['query']),
     propose: IDL.Func([ProposalContent], [Result_2], []),
     quit: IDL.Func([], [Result_4], []),
     update_dao_info: IDL.Func([DaoInfo], [Result], []),
     user_info: IDL.Func([IDL.Opt(IDL.Principal)], [Result_4], []),
-    vote: IDL.Func([UserVoteArgs], [Result_6], [])
+    vote: IDL.Func([UserVoteArgs], [Result_8], [])
   });
 };
 export const init = ({ IDL }) => {
--- src/nnsdao/types.ts
+++ src/nnsdao/types.ts
@@ -1,6 +1,13 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export interface CanisterStatusResponse {
+  status: Status;
+  memory_size: bigint;
+  cycles: bigint;
+  settings: DefiniteCanisterSettings;
+  module_hash: [] | [Array<number>];
+}
 export interface DaoInfo {
   option: [] | [Array<[string, string]>];
   name: string;
@@ -9,6 +16,12 @@ export interface DaoInfo {
   intro: string;
   avatar: string;
   poster: string;
+}
+export interface DefiniteCanisterSettings {
+  freezing_threshold: bigint;
+  controllers: Array<Principal>;
+  memory_allocation: bigint;
+  compute_allocation: bigint;
 }
 export interface JoinDaoParams {
   nickname: string;
@@ -22,6 +35,7 @@ export interface MemberItems {
   social: Array<Social>;
   join_at: bigint;
   intro: string;
+  last_visit_at: bigint;
   status_code: number;
   avatar: string;
 }
@@ -31,6 +45,7 @@ export interface Proposal {
   content: string;
   vote_data: Array<[Principal, Votes]>;
   end_time: bigint;
+  start_time: bigint;
   timestamp: bigint;
   property: [] | [Array<[string, string]>];
   proposer: Principal;
@@ -40,7 +55,12 @@ export interface ProposalContent {
   title: string;
   content: string;
   end_time: bigint;
+  start_time: bigint;
   property: [] | [Array<[string, string]>];
+}
+export interface ProposalLog {
+  pending: Array<bigint>;
+  finished: Array<[bigint, Result_6]>;
 }
 export type ProposalState =
   | { Failed: string }
@@ -49,17 +69,30 @@ export type ProposalState =
   | { Rejected: null }
   | { Succeeded: null }
   | { Accepted: null };
+export type RejectionCode =
+  | { NoError: null }
+  | { CanisterError: null }
+  | { SysTransient: null }
+  | { DestinationInvalid: null }
+  | { Unknown: null }
+  | { SysFatal: null }
+  | { CanisterReject: null };
 export type Result = { Ok: DaoInfo } | { Err: string };
-export type Result_1 = { Ok: string } | { Err: string };
+export type Result_1 =
+  | { Ok: [CanisterStatusResponse] }
+  | { Err: [RejectionCode, string] };
 export type Result_2 = { Ok: Proposal } | { Err: string };
 export type Result_3 = { Ok: Array<[bigint, Proposal]> } | { Err: string };
 export type Result_4 = { Ok: MemberItems } | { Err: string };
 export type Result_5 = { Ok: Array<MemberItems> } | { Err: string };
-export type Result_6 = { Ok: null } | { Err: string };
+export type Result_6 = { Ok: string } | { Err: string };
+export type Result_7 = { Ok: ProposalLog } | { Err: string };
+export type Result_8 = { Ok: null } | { Err: string };
 export interface Social {
   key: string;
   link: string;
 }
+export type Status = { stopped: null } | { stopping: null } | { running: null };
 export interface UserVoteArgs {
   id: bigint;
   principal: [] | [Principal];
@@ -68,14 +101,16 @@ export interface UserVoteArgs {
 export type Votes = { No: bigint } | { Yes: bigint };
 export interface _SERVICE {
   dao_info: ActorMethod<[], Result>;
-  get_handled_proposal: ActorMethod<[], Array<[bigint, Result_1]>>;
+  dao_status: ActorMethod<[], Result_1>;
+  get_owner: ActorMethod<[], Array<Principal>>;
   get_proposal: ActorMethod<[bigint], Result_2>;
   get_proposal_list: ActorMethod<[], Result_3>;
   join: ActorMethod<[JoinDaoParams], Result_4>;
   member_list: ActorMethod<[], Result_5>;
+  proposal_heartbeat_log: ActorMethod<[], Result_7>;
   propose: ActorMethod<[ProposalContent], Result_2>;
   quit: ActorMethod<[], Result_4>;
   update_dao_info: ActorMethod<[DaoInfo], Result>;
   user_info: ActorMethod<[[] | [Principal]], Result_4>;
-  vote: ActorMethod<[UserVoteArgs], Result_6>;
+  vote: ActorMethod<[UserVoteArgs], Result_8>;
 }
