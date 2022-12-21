bd158675b9f53080e14ab911dfa1d4cc0b90a2ac
neptune0xf <null>
2022-12-21 14:49:57 +0000

    🔄 Synced file(s) with NnsDao/nnsdao-nid

--- src/nid/types.ts
+++ src/nid/types.ts
@@ -2,8 +2,8 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
 export interface BasicUserInfo {
-  nid: bigint;
   nickname: string;
+  social: Array<[string, string]>;
   intro: string;
   avatar: string;
 }
@@ -30,24 +30,28 @@ export type StakeItemStatus =
   | { Expired: null };
 export interface TotalUserInfo {
   log: Array<UserLog>;
-  nid: bigint;
+  nid: string;
   nickname: string;
+  social: Array<[string, string]>;
   level: bigint;
   credit: bigint;
   stake: Array<StakeItem>;
   badge: Array<string>;
-  wallet: Array<[bigint, string, string]>;
+  wallet: Array<[string, string, string]>;
+  last_login_at: bigint;
   intro: string;
   avatar: string;
 }
 export interface UserItem {
   log: Array<UserLog>;
-  nid: bigint;
+  nid: string;
   nickname: string;
+  social: Array<[string, string]>;
   level: bigint;
   credit: bigint;
   stake: Array<StakeItem>;
   badge: Array<string>;
+  last_login_at: bigint;
   intro: string;
   avatar: string;
 }
@@ -59,11 +63,11 @@ export interface UserLog {
 export interface _SERVICE {
   add_admin: ActorMethod<[Principal], Result>;
   add_stake: ActorMethod<[StakeItem], Result_1>;
-  bind_wallet: ActorMethod<[[bigint, string, string]], Result>;
-  get_bind_wallet: ActorMethod<[], Array<[bigint, string, string]>>;
+  bind_wallet: ActorMethod<[[string, string, string]], Result>;
+  get_bind_wallet: ActorMethod<[], Array<[string, string, string]>>;
   login: ActorMethod<[string], Result_2>;
   metadata: ActorMethod<[], Result_3>;
   system_time: ActorMethod<[], bigint>;
-  update_user_info: ActorMethod<[BasicUserInfo], Result>;
+  update_user_info: ActorMethod<[BasicUserInfo], Result_2>;
   user_info: ActorMethod<[], Result_2>;
 }
