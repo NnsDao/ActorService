462e6134ac6b194f815f4a008a0d66160baf1567
suno <42191310+DevSuno@users.noreply.github.com>
2023-01-02 22:59:51 +0800

    fix:podcast

--- src/podcast/index.ts
+++ src/podcast/index.ts
@@ -1,5 +1,109 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({});
+  const SetBaseInfoRes = IDL.Record({
+    describe: IDL.Text,
+    cover_image: IDL.Text,
+    icon: IDL.Text,
+    name: IDL.Text,
+  });
+  const Categories = IDL.Variant({ Default: IDL.Null });
+  const Language = IDL.Variant({
+    Korean: IDL.Null,
+    Japanese: IDL.Null,
+    Chinese: IDL.Null,
+    English: IDL.Null,
+    Arabic: IDL.Null,
+  });
+  const PodcastIterm = IDL.Record({
+    tag: IDL.Vec(IDL.Text),
+    categories: Categories,
+    status: IDL.Bool,
+    describe: IDL.Text,
+    title: IDL.Text,
+    hosts: IDL.Opt(IDL.Principal),
+    cover_image: IDL.Text,
+    link: IDL.Text,
+    create_at: IDL.Nat64,
+    language: Language,
+    update_at: IDL.Nat64,
+    show_note: IDL.Text,
+    guests: IDL.Vec(IDL.Principal),
+    sub_title: IDL.Text,
+  });
+  const RejectionCode = IDL.Variant({
+    NoError: IDL.Null,
+    CanisterError: IDL.Null,
+    SysTransient: IDL.Null,
+    DestinationInvalid: IDL.Null,
+    Unknown: IDL.Null,
+    SysFatal: IDL.Null,
+    CanisterReject: IDL.Null,
+  });
+  const Result = IDL.Variant({
+    Ok: IDL.Null,
+    Err: IDL.Tuple(RejectionCode, IDL.Text),
+  });
+  const CanisterStatusType = IDL.Variant({
+    stopped: IDL.Null,
+    stopping: IDL.Null,
+    running: IDL.Null,
+  });
+  const DefiniteCanisterSettings = IDL.Record({
+    freezing_threshold: IDL.Nat,
+    controllers: IDL.Vec(IDL.Principal),
+    memory_allocation: IDL.Nat,
+    compute_allocation: IDL.Nat,
+  });
+  const CanisterStatusResponse = IDL.Record({
+    status: CanisterStatusType,
+    memory_size: IDL.Nat,
+    cycles: IDL.Nat,
+    settings: DefiniteCanisterSettings,
+    idle_cycles_burned_per_day: IDL.Nat,
+    module_hash: IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const Result_1 = IDL.Variant({
+    Ok: IDL.Tuple(CanisterStatusResponse),
+    Err: IDL.Tuple(RejectionCode, IDL.Text),
+  });
+  const Info = IDL.Record({
+    describe: IDL.Text,
+    cover_image: IDL.Text,
+    icon: IDL.Text,
+    name: IDL.Text,
+    create_at: IDL.Nat64,
+    update_at: IDL.Nat64,
+  });
+  const SocialLink = IDL.Record({
+    twitter: IDL.Text,
+    blog: IDL.Text,
+    instagram: IDL.Text,
+    email: IDL.Text,
+    distrikt: IDL.Text,
+    dmail: IDL.Text,
+    dscvr: IDL.Text,
+    telegram: IDL.Text,
+    github: IDL.Text,
+    openchat: IDL.Text,
+  });
+  const Result_2 = IDL.Variant({ Ok: IDL.Null, Err: IDL.Text });
+  return IDL.Service({
+    add_owner: IDL.Func([IDL.Principal], [], []),
+    change_admin: IDL.Func([IDL.Principal], [], []),
+    create_base_info: IDL.Func([SetBaseInfoRes], [], []),
+    create_podcast: IDL.Func([PodcastIterm], [], []),
+    delete_owner: IDL.Func([IDL.Principal], [], []),
+    deposit: IDL.Func([IDL.Principal, IDL.Nat], [Result], []),
+    get_admin: IDL.Func([], [IDL.Opt(IDL.Principal)], ['query']),
+    get_canister_status: IDL.Func([IDL.Principal], [Result_1], []),
+    get_owner: IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    get_podcast: IDL.Func([IDL.Nat64], [IDL.Opt(PodcastIterm)], ['query']),
+    get_podcast_base_info: IDL.Func([], [Info], ['query']),
+    get_podcast_list: IDL.Func([], [IDL.Vec(IDL.Tuple(IDL.Nat64, PodcastIterm))], ['query']),
+    get_social_link: IDL.Func([], [SocialLink], ['query']),
+    set_social_link: IDL.Func([SocialLink], [], []),
+    update_base_info: IDL.Func([SetBaseInfoRes], [], []),
+    update_podcast: IDL.Func([IDL.Nat64, PodcastIterm], [Result_2], []),
+  });
 };
 export const init = ({ IDL }) => {
   return [];
--- src/podcast/types.ts
+++ src/podcast/types.ts
@@ -1,4 +1,89 @@
 import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {}
+export interface CanisterStatusResponse {
+  status: CanisterStatusType;
+  memory_size: bigint;
+  cycles: bigint;
+  settings: DefiniteCanisterSettings;
+  idle_cycles_burned_per_day: bigint;
+  module_hash: [] | [Array<number>];
+}
+export type CanisterStatusType = { stopped: null } | { stopping: null } | { running: null };
+export type Categories = { Default: null };
+export interface DefiniteCanisterSettings {
+  freezing_threshold: bigint;
+  controllers: Array<Principal>;
+  memory_allocation: bigint;
+  compute_allocation: bigint;
+}
+export interface Info {
+  describe: string;
+  cover_image: string;
+  icon: string;
+  name: string;
+  create_at: bigint;
+  update_at: bigint;
+}
+export type Language = { Korean: null } | { Japanese: null } | { Chinese: null } | { English: null } | { Arabic: null };
+export interface PodcastIterm {
+  tag: Array<string>;
+  categories: Categories;
+  status: boolean;
+  describe: string;
+  title: string;
+  hosts: [] | [Principal];
+  cover_image: string;
+  link: string;
+  create_at: bigint;
+  language: Language;
+  update_at: bigint;
+  show_note: string;
+  guests: Array<Principal>;
+  sub_title: string;
+}
+export type RejectionCode =
+  | { NoError: null }
+  | { CanisterError: null }
+  | { SysTransient: null }
+  | { DestinationInvalid: null }
+  | { Unknown: null }
+  | { SysFatal: null }
+  | { CanisterReject: null };
+export type Result = { Ok: null } | { Err: [RejectionCode, string] };
+export type Result_1 = { Ok: [CanisterStatusResponse] } | { Err: [RejectionCode, string] };
+export type Result_2 = { Ok: null } | { Err: string };
+export interface SetBaseInfoRes {
+  describe: string;
+  cover_image: string;
+  icon: string;
+  name: string;
+}
+export interface SocialLink {
+  twitter: string;
+  blog: string;
+  instagram: string;
+  email: string;
+  distrikt: string;
+  dmail: string;
+  dscvr: string;
+  telegram: string;
+  github: string;
+  openchat: string;
+}
+export interface _SERVICE {
+  add_owner: (arg_0: Principal) => Promise<undefined>;
+  change_admin: (arg_0: Principal) => Promise<undefined>;
+  create_base_info: (arg_0: SetBaseInfoRes) => Promise<undefined>;
+  create_podcast: (arg_0: PodcastIterm) => Promise<undefined>;
+  delete_owner: (arg_0: Principal) => Promise<undefined>;
+  deposit: (arg_0: Principal, arg_1: bigint) => Promise<Result>;
+  get_admin: () => Promise<[] | [Principal]>;
+  get_canister_status: (arg_0: Principal) => Promise<Result_1>;
+  get_owner: () => Promise<Array<Principal>>;
+  get_podcast: (arg_0: bigint) => Promise<[] | [PodcastIterm]>;
+  get_podcast_base_info: () => Promise<Info>;
+  get_podcast_list: () => Promise<Array<[bigint, PodcastIterm]>>;
+  get_social_link: () => Promise<SocialLink>;
+  set_social_link: (arg_0: SocialLink) => Promise<undefined>;
+  update_base_info: (arg_0: SetBaseInfoRes) => Promise<undefined>;
+  update_podcast: (arg_0: bigint, arg_1: PodcastIterm) => Promise<Result_2>;
+}
